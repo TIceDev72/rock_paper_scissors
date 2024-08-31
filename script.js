4064adff5aa050e3fc05ca98c2ae12a6488b3981
JustixDev <171641141+justixDev@users.noreply.github.com>
2024-08-31 02:30:22 +0100

    Finish playRound function and Add variables
    - completed the playRound function
    - Added variables to hold the score for both player and pcs
    
    This will enable the program determine the winner and store the scores
    correctly.

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-"use strict"
+'use strict'
 function getComputerChoice() {
-  const choiceList = ["rock", "paper", "scissors"]
+  const choiceList = ['rock', 'paper', 'scissors']
   let computerChoice = choiceList[Math.floor(Math.random() * 3)]
   return computerChoice
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Enter Your choice:")
+  let humanChoice = prompt('Enter Your choice:')
   return humanChoice
+}
+
+function playRound(humanChoice, computerChoice) {
+  let lowerCaseHumChoice = humanChoice.toLowerCase()
+  switch (lowerCaseHumChoice) {
+    case 'rock':
+      if (computerChoice !== lowerCaseHumChoice && computerChoice !== 'paper') {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        ++humanScore
+      } else if (computerChoice === lowerCaseHumChoice) {
+        console.log(
+          `It's a tie!\nYour choice "${humanChoice}" and pc's choice "${computerChoice}" are thesame.`
+        )
+      } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}. Pc Won!`)
+        ++computerScore
+      }
+      break
+    case 'paper':
+      if (
+        computerChoice !== lowerCaseHumChoice &&
+        computerChoice !== 'scissors'
+      ) {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        ++humanScore
+      } else if (computerChoice === lowerCaseHumChoice) {
+        console.log(
+          `It's a tie!\nYour choice "${humanChoice}" and pc's choice "${computerChoice}" are thesame.`
+        )
+      } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}. Pc Won!`)
+        ++computerScore
+      }
+      break
+    case 'scissors':
+      if (computerChoice !== lowerCaseHumChoice && computerChoice !== 'rock') {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        ++humanScore
+      } else if (computerChoice === lowerCaseHumChoice) {
+        console.log(
+          `It's a tie!\nYour choice "${humanChoice}" and pc's choice "${computerChoice}" are thesame.`
+        )
+      } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}. Pc Won!`)
+        ++computerScore
+      }
+      break
+  }
 }
 
 let humanScore = 0
 let computerScore = 0
 
-function playRound(humanChoice, computerChoice) {}
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
+
+console.log('Your score is: ' + humanScore)
+console.log('Computer score is: ' + computerScore)
