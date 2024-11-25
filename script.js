ab14ed6fff675e44c5e365f6b3d7d7885d894193
JustixDev <171641141+justixDev@users.noreply.github.com>
2024-11-25 22:30:16 +0100

    Update
    - Changed variable names
    - Changed the "playRound" function logic
    - Changed the "playGame" function logic
    
    These changes will make the game flow properly.

--- script.js
+++ script.js
@@ -1,69 +1,74 @@
-'use strict'
-// list of available choices
-const choices = ['rock', 'paper', 'scissors']
+"use strict";
+const choices = ["rock", "paper", "scissors"];
 
 // Get computer's choice
 function getComputerChoice() {
-  let selectedChoice = choices[Math.floor(Math.random() * 3)]
-  return selectedChoice
+  let selectedChoice = choices[Math.floor(Math.random() * 3)];
+  return selectedChoice;
 }
+
 // Get the human's choice
 function getHumanChoice() {
-  let selectedChoice
+  let result;
 
   while (true) {
-    selectedChoice = prompt(
-      'Enter Your choice (rock, paper, and scissors):'
-    ).toLocaleLowerCase()
+    result = prompt(
+      "Enter Your choice (rock, paper, and scissors):"
+    ).toLowerCase();
 
-    if (choices.includes(selectedChoice)) {
-      break
+    if (choices.includes(result)) {
+      break;
     } else {
-      console.log('Invalid choice! Try agin.')
+      console.log("Invalid choice! Try agin.");
     }
   }
-  return selectedChoice
+  return result;
 }
 
 // play a round
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    console.log(`It's a tie! Both are ${humanChoice}`)
-    return 'tie'
+    console.log(`Tie! Both are ${humanChoice}`);
+    return "tie";
   }
 
   if (
-    (humanChoice === 'rock' && computerChoice === 'scissors') ||
-    (humanChoice === 'paper' && computerChoice === 'rock') ||
-    (humanChoice === 'scissors' && computerChoice == 'paper')
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice == "paper")
   ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-    return 'human'
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    return "human";
   } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-    return 'computer'
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    return "computer";
   }
 }
 
 function playGame() {
-  let humanScore = 0
-  let computerScore = 0
+  let humanScore = 0;
+  let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice()
-    let computerSelection = getComputerChoice()
-    let result = playRound(humanSelection, computerSelection)
+  for (let i = 0, n = 5; i < n; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    let result = playRound(humanSelection, computerSelection);
 
-    if (result === 'human') {
-      humanScore++
-    } else if (result === 'computer') {
-      computerScore++
+    if (result === "tie") {
+      ++n;
+      continue;
+    } else if (result === "computer") {
+      ++computerScore;
+    } else {
+      ++humanScore;
     }
   }
 
-  console.log('Final Score')
-  console.log('Your score: ' + humanScore)
-  console.log('Computer score: ' + computerScore)
+  console.log(
+    `Final Score:\n
+    Your score: ${humanScore}\n
+    Computer score: ${computerScore} `
+  );
 }
 
-playGame()
+playGame();
