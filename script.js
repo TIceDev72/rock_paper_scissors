cde5053223f275dc287c6d56ad55b5a372a98e84
JustixDev <171641141+justixDev@users.noreply.github.com>
2024-12-04 03:00:54 +0100

    Update
    - Added a confirmation dialog box
    - Changed colors for background and text

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const choices = ["rock", "paper", "scissors"];
 
+const gameStartDialogBox = document.querySelector(
+  ".game-start-overlay .dialog-box"
+);
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const overlayResultDisplay = document.querySelector(".overlay-result-display");
@@ -10,59 +13,80 @@ const overlay = document.querySelector(".overlay");
 const playerScoreDisplay = document.querySelector(".player-score-display");
 const computerScoreDisplay = document.querySelector(".computer-score-display");
 const tryAgainButton = document.querySelector(".try-again-button");
+const fetchingDataMessage = document.querySelector(".computer-mesg");
 
 let mainResultDisplay = document.querySelector(".main-result-display");
 
 let computerScore = 0;
 let playerScore = 0;
 
-choicesContainer.addEventListener("click", (e) => {
-  let button = e.target
-    .getAttribute("class")
-    .slice(0, e.target.getAttribute("class").indexOf("-"));
+// Show a dialog box when page loads or is refreshed
+gameStartDialogBox.addEventListener("click", (button) => {
+  let buttonClicked = button.target.getAttribute("class");
 
-  if (button !== "choice") {
+  // If yes is clicked remove dialog box and overlay
+  if (buttonClicked === "yes-button") {
+    gameStartDialogBox.parentNode.remove();
+    // Else insert the error code in the in the HTML body tag
+  } else if (buttonClicked === "no-button") {
+    document.body.innerHTML = `<code> Game cancelled! Refresh your browser to play game.</code>`;
+  }
+});
+
+// Dispatch an event to the document object if any button is clicked
+choicesContainer.addEventListener("click", (button) => {
+  let buttonClicked = button.target.getAttribute("class");
+
+  fetchingDataMessage.textContent = "Computer is deciding...";
+  setTimeout(() => {
+    fetchingDataMessage.textContent = "";
+
+    if (!choices.includes(buttonClicked)) return;
+
     let playerMadeChoiceEvent = new CustomEvent("player-made-choice", {
       detail: {
-        playerChoice: button,
+        playerChoice: buttonClicked,
         computerChoice: choices[Math.floor(Math.random() * 3)],
       },
     });
 
     document.dispatchEvent(playerMadeChoiceEvent);
-  }
+  }, 200);
 });
 
+// Call functions to update different parts of the game
 document.addEventListener("player-made-choice", (event) => {
-  updateChoices(event.detail.playerChoice, event.detail.computerChoice);
-
-  let winner = getWinner(
+  let [playerSelection, computerSelection] = [
     event.detail.playerChoice,
-    event.detail.computerChoice
-  );
+    event.detail.computerChoice,
+  ];
+
+  updateChoices(playerSelection, computerSelection);
+
+  let winner = getWinner(playerSelection, computerSelection);
 
   updateScores(winner);
   announceWinner(playerScore, computerScore);
 });
 
 tryAgainButton.addEventListener("click", () => {
-  tryAgainButton.parentNode.classList.toggle("show-overlay");
+  overlay.classList.toggle("show-overlay");
 
   clearData();
 });
 
-function updateChoices(pChoice, cChoice) {
-  playerDisplay.textContent = "PLAYER: " + pChoice;
-  computerDisplay.textContent = "COMPUTER: " + cChoice;
+function updateChoices(pSelection, cSelection) {
+  playerDisplay.textContent = "PLAYER: " + pSelection;
+  computerDisplay.textContent = "COMPUTER: " + cSelection;
 }
 
-function getWinner(pChoice, cChoice) {
-  if (pChoice === cChoice) {
+function getWinner(pSelection, cSelection) {
+  if (pSelection === cSelection) {
     return "tie";
   } else if (
-    (pChoice === "rock" && cChoice === "scissors") ||
-    (pChoice === "scissors" && cChoice === "paper") ||
-    (pChoice === "paper" && cChoice === "rock")
+    (pSelection === "rock" && cSelection === "scissors") ||
+    (pSelection === "scissors" && cSelection === "paper") ||
+    (pSelection === "paper" && cSelection === "rock")
   ) {
     return "player";
   } else {
@@ -85,12 +109,14 @@ function updateScores(winner) {
 }
 
 function announceWinner(pScore, cScore) {
-  if (pScore === 5) {
+  if (pScore >= 5) {
     overlayResultDisplay.textContent = "YOU WIN! 🫵";
     overlay.classList.toggle("show-overlay");
-  }
-  if (cScore === 5) {
+  } else if (cScore >= 5) {
     overlayResultDisplay.textContent = "YOU LOST! 🤦";
+    overlay.classList.toggle("show-overlay");
+  } else if (pScore >= 5 && cScore >= 5) {
+    overlayResultDisplay.textContent = "--DRAW--";
     overlay.classList.toggle("show-overlay");
   }
 }
